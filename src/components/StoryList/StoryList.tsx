d861d413e5d2a5e5b7d80432bd42442db9b14e1f
Levente Hegedüs <levi.hegedus92@gmail.com>
2025-01-08 00:57:10 +0100

    design fixes, table header fixes

--- src/components/StoryList/StoryList.tsx
+++ src/components/StoryList/StoryList.tsx
@@ -35,44 +35,49 @@ const StoryList: React.FC = () => {
 
   return (
     <div className="bg-off-white rounded-b-lg w-full h-full">
-      <header className="flex justify-between items-center flex-wrap py-4 px-[30px] gap-4">
+      <header className="flex justify-between items-center flex-wrap py-4 px-[30px] gap-4 mb-5">
         <h1 className="text-3xl font-semibold w-full tracking-[.01em] text-dark-primary">
           Stories
         </h1>
 
-        <div className="flex gap-4 w-full gap-[30px]">
-          <div className="relative flex w-[376px] h-9 border-gray-light border rounded-md bg-white overflow-hidden">
-            <input
-              type="search"
-              placeholder="Search"
-              className="w-full pl-3 pr-4 py-2 bg-white text-sm leading-5 -tracking-[.01em]"
-              value={filters.search}
-              onChange={(e) =>
-                setFilters({ ...filters, search: e.target.value })
-              }
-            />
-            <div className="w-9 h-9 bg-gray-light-search flex items-center justify-center">
-              <Search size={12} />
+        <div className="flex gap-4 w-full gap-[30px] justify-between">
+          <div className="flex gap-[30px]">
+            <div className="relative flex w-[376px] h-9 border-gray-light border rounded-md bg-white overflow-hidden">
+              <input
+                type="search"
+                placeholder="Search"
+                className="w-full pl-3 pr-4 py-2 bg-white text-sm leading-5 -tracking-[.01em]"
+                value={filters.search}
+                onChange={(e) =>
+                  setFilters({ ...filters, search: e.target.value })
+                }
+              />
+              <div className="w-9 h-9 bg-gray-light-search flex items-center justify-center">
+                <Search size={12} />
+              </div>
             </div>
-          </div>
-          <div className="relative w-48">
-            <select
-              className="w-full appearance-none bg-white border rounded-md px-4 py-2 pr-10 h-9"
-              value={filters.status}
-              onChange={(e) =>
-                setFilters({ ...filters, status: e.target.value })
-              }
-            >
-              <option>All Statuses</option>
-              <option>Draft</option>
-              <option>Scheduled</option>
-              <option>Live</option>
-              <option>Past</option>
-            </select>
-            <ChevronDown
-              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400"
-              size={20}
-            />
+            <div className="relative w-[200px]">
+              <select
+                className="w-full appearance-none bg-white border border-gray-light text-dark-primary rounded-md px-3 pr-10 h-9"
+                value={filters.status}
+                onChange={(e) =>
+                  setFilters({ ...filters, status: e.target.value })
+                }
+              >
+                <option>All Statuses</option>
+                <option>Draft</option>
+                <option>Scheduled</option>
+                <option>Live</option>
+                <option>Past</option>
+              </select>
+              <ChevronDown
+                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-dark-primary -tracking-[.01em]"
+                size={20}
+              />
+            </div>
+            <div className="text-dark-primary text-sm leading-5 flex items-center -tracking-[.01em]">
+              Showing {1} to {20} of {176}
+            </div>
           </div>
           <Button
             text="New Story"
@@ -82,16 +87,17 @@ const StoryList: React.FC = () => {
           />
         </div>
       </header>
-
       <div className="w-full -mt-0.5 max-w-full overflow-auto">
-        <div className="border-b flex">
-          <div className="text-left py-3 px-4 flex-1">Title</div>
-          <div className="text-left py-3 px-4 flex-1">Pages</div>
-          <div className="text-left py-3 px-4 flex-1">Last Modified</div>
-          <div className="text-left py-3 px-4 flex-1">Status</div>
-          <div className="text-left py-3 px-4 flex-1">Live From</div>
-          <div className="text-left py-3 px-4 flex-1">Ends</div>
-          <div className="text-left py-3 px-4 flex-1">Actions</div>
+        <div className="border-b flex text-dark-primary flex items-center justify-between font-semibold text-dark-primary opacity-75 text-sm leading-5 pb-2">
+          <div className="pl-[30px]">Title</div>
+          <div className="flex">
+            <div className="pl-[10px] w-[273px]">Pages</div>
+            <div className="pl-[10px] w-[165px]">Last Modified</div>
+            <div className="pl-[10px] w-[111px]">Status</div>
+            <div className="pl-[10px] w-[165px]">Live From</div>
+            <div className="pl-[10px] w-[165px]">Ends</div>
+            <div className="pl-[10px] w-[157px]"></div>
+          </div>
         </div>
         <div>
           {stories.map((story) => (
