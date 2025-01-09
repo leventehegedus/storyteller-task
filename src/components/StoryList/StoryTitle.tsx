555d0a9731a6be77bced8e099e4431e42e077423
Levente Hegedüs <levi.hegedus92@gmail.com>
2025-01-09 23:38:27 +0100

    long text overflow fix

--- src/components/StoryList/StoryTitle.tsx
+++ src/components/StoryList/StoryTitle.tsx
@@ -7,11 +7,11 @@ interface StoryTitleProps {
 
 const StoryTitle: React.FC<StoryTitleProps> = ({ title, subtitle }) => (
   <div className="font-medium text-base leading-5 text-ellipsis">
-    <div className="text-blue-primary text-nowrap -tracking-[.01em] truncate max-w-[202px] lg:max-w-[222px] 2xl:max-w-[calc(100vw-1336px)]">
+    <div className="text-blue-primary text-nowrap -tracking-[.01em] truncate max-w-[162px] lg:max-w-[222px] 2xl:max-w-[calc(100vw-1336px)]">
       {title}
     </div>
     {subtitle && (
-      <div className="text-gray text-nowrap -tracking-[.01em] truncate max-w-[202px] lg:max-w-[222px] 2xl:max-w-[calc(100vw-1336px)]">
+      <div className="text-gray text-nowrap -tracking-[.01em] truncate max-w-[162px] lg:max-w-[222px] 2xl:max-w-[calc(100vw-1336px)]">
         {subtitle}
       </div>
     )}
